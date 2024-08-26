6c4c4e9007ceb827f6da2594f8793f37c454e950
GabrielPeixotoValeriano <bielop123@gmail.com>
2024-08-26 20:07:12 -0300

    Projeto Furia Landing Page

--- script.js
+++ script.js
@@ -1,57 +1,75 @@
-    // Formulário
-    function Submeter (){
-        const nome = document.getElementById('nome').value;
-        const idade = document.getElementById('idade').value;
-        const email = document.getElementById('email').value;
-        const jogador = document.getElementById('jogador').value;
-        const agente = document.getElementById('agente').value;
-        const ranking = document.getElementById('ranking').value;
-        const mensagem = document.getElementById('mensagem');
-    //Validação das informações do formulario
-    
+// Formulário
+function Submeter() {
+    const nome = document.getElementById('nome').value;
+    const idade = document.getElementById('idade').value;
+    const email = document.getElementById('email').value;
+    const jogador = document.getElementById('jogador').value;
+    const agente = document.getElementById('agente').value;
+    const ranking = document.getElementById('ranking').value;
+    const mensagem = document.getElementById('modalMensagem');
+
+    // Validação das informações do formulário
     mensagem.innerHTML = "";
 
-    if (nome == "") {
-        mensagem.innerHTML += `Por favor preencha seu nome.`;
+    if (nome === "") {
+        mensagem.innerHTML += `Por favor preencha seu nome.<br>`;
     }
 
-    if (idade == "") {
-        mensagem.innerHTML += `<br> Por favor preencha sua idade.`;
+    if (idade === "") {
+        mensagem.innerHTML += `Por favor preencha sua idade.<br>`;
     }
 
-    if (email == "") {
-        mensagem.innerHTML += `<br> Por favor preencha seu email.`;
+    if (email === "") {
+        mensagem.innerHTML += `Por favor preencha seu email.<br>`;
     }
 
-    if (jogador == "vazio") {
-        mensagem.innerHTML += `<br> Por favor selecione seu jogador favorito.`;
+    if (jogador === "vazio") {
+        mensagem.innerHTML += `Por favor selecione seu jogador favorito.<br>`;
     }
 
-    if (agente == "vazio") {
-        mensagem.innerHTML += `<br> Por favor selecione seu agente favorito.`;
+    if (agente === "vazio") {
+        mensagem.innerHTML += `Por favor selecione seu agente favorito.<br>`;
     }
 
-    if (ranking == "vazio") {
-        mensagem.innerHTML += `<br> Por favor selecione seu ranking.`;
+    if (ranking === "vazio") {
+        mensagem.innerHTML += `Por favor selecione seu ranking.<br>`;
     }
 
     if (mensagem.innerHTML.length > 0) {
+        document.getElementById('validationModal').style.display = 'block';
         return;
     }
 
     // Mensagem de confirmação
-    const menssagem_confirmacao = document.createElement('div');
-    menssagem_confirmacao.className = 'confirmation-message';
-    menssagem_confirmacao.innerHTML = `
+    const mensagem_confirmacao = document.createElement('div');
+    mensagem_confirmacao.className = 'confirmation-message';
+    mensagem_confirmacao.innerHTML = `
         <h2>Obrigado!</h2>
-        <p>Seu e-mail: ${email}<br>Seu nome: ${nome}<br>Sua idade: ${idade}<br>Seu jogador favorito: ${jogador}<br>Seu agente favorito: ${agente}<br>Seu ranking: ${ranking}</p>
-        `;
+        <p>Seu e-mail: ${email}</p>
+        <p>Seu nome: ${nome}<p>Sua idade: ${idade}</p>
+        <p>Seu jogador favorito: ${jogador}</p>
+        <p>Seu agente favorito: ${agente}</p>
+        <p>Seu ranking: ${ranking}</p>
+    `;
 
     const quadro_confirmacao = document.querySelector('.formulario-landpage');
     quadro_confirmacao.innerHTML = '';
-    quadro_confirmacao.appendChild(menssagem_confirmacao);
+    quadro_confirmacao.appendChild(mensagem_confirmacao);
+}
 
-    // document.getElementById('closeConfirmation').addEventListener('click', () => {
-    //     document.getElementById('popup').style.display = 'none';
-    // });
+window.onload = function() {
+    // Fechar o modal 
+    document.querySelector('.modal-content .close').addEventListener('click', function() {
+        document.getElementById('validationModal').style.display = 'none';
+    });
+
+    // Fechar o modal ao clicar fora da área do modal
+    window.onclick = function(event) {
+        const modal = document.getElementById('validationModal');
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
     };
+};
+
+
